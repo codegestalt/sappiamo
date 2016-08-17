c0b288e3daa5d37c740c64cf4853bdfb0fe4dbbb
Rodrigo Haenggi <rodrigo@codegestalt.com>
2016-08-17 10:10:56 +0200

    title model and tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sappiamo'
-
 require 'minitest/autorun'
+require 'webmock/minitest'
+require 'vcr'
+
+env = File.join('env.rb')
+load(env) if File.exists?(env)
+
+VCR.configure do |c|
+  c.cassette_library_dir = "test/fixtures"
+  c.hook_into :webmock
+  c.filter_sensitive_data("<API_KEY>"){ ENV['SAPPIAMO_API_TOKEN'] }
+end
--- test/title/title_test.rb
+++ test/title/title_test.rb
@@ -4,4 +4,38 @@ class SappiamoTitleTest < Minitest::Test
   def test_exists
     assert Sappiamo::Title
   end
+
+  def test_returns_single_title
+    VCR.use_cassette('one_title') do
+      title = Sappiamo::Title.find("00366E69-16A4-44EE-8F41-AD2B5C9BA44B")
+      assert_equal Sappiamo::Title, title.class
+
+      assert_equal "00366E69-16A4-44EE-8F41-AD2B5C9BA44B", title.id
+      assert_equal "P1600621", title.product_nr
+
+      assert_equal "Veltliner Fussreisen", title.title
+      assert_equal "Zwischen Bündnerpässen und Bergamasker Alpen", title.subtitle
+      assert_equal "Bauer/Frischknecht, Veltliner Fu\u00dfreisen", title.description
+
+      assert_equal "DE", title.language
+
+      assert_equal "10.07.2007", title.published
+      assert_equal 414, title.pages
+
+      assert_equal "Lesewanderbuch", title.category
+      assert_equal "1600", title.category_code
+
+      assert_equal "9783858693495", title.ean
+
+      assert_equal "978-3-85869-349-5", title.isbn
+
+      assert_equal 780, title.stock_ch
+      assert_equal nil, title.stock_de
+
+      assert_equal 19.9, title.price_chf
+      assert_equal 17.5, title.price_eur
+
+      assert_equal "4., Aufl. 10.07.2007", title.edition_text
+    end
+  end
 end
